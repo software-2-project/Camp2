b99856809dae30cc94b7b8602b996ebf2eba3060
asmaaessam <42878434+asmaaessam@users.noreply.github.com>
2019-04-18 10:07:39 +0200

    Add files via upload

---
+++ Models/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+include "connection.php";
+include_once "app_user.php";
+
+class admin extends App_user {
+
+    public function Add_admin($fname , $lname , $phone , $salary , $email , $password ){
+
+      $add_query = "INSERT INTO `admin`(`ad_fname`, `ad_lname`, `ad_phone`, `ad_salary`, `ad_email`, `ad_password`)
+      VALUES ('$fname' , '$lname' , '$phone' , '$salary' , '$email' , '$password')";
+      $val = mysqli_query($GLOBALS['conn'], $add_query);
+}
+}
